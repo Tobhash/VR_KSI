96d0069a3aed704e5c238820ebb27c4618f1725d
Tobiasz Porębski <tobiasz.porebski@gmail.com>
2016-11-25 10:16:58 +0100

    Warsztaty 24.11
    Zapoznanie ze środowiskiem i narzędziami zawartymi w paczce
    GoogleVRUnity SDK

---
+++ Assets/myVR/Scripts/Rotator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    public float speed;
+    public GameObject player;
+
+    private Vector3 pointToRotate;
+
+	// Use this for initialization
+	void Start () {
+        pointToRotate = player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(pointToRotate, Vector3.up, speed * Time.deltaTime);
+	}
+
+    public void shutDown() {
+        gameObject.SetActive(false);
+    }
+}

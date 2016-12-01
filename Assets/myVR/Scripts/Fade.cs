d6d4f269cad0a2b6d7f36442a7cdb7dbb5945cff
Tobiasz Porębski <tobiasz.porebski@gmail.com>
2016-12-01 10:23:18 +0100

    przez 1.12
    Przygotowanie pod zajecia 1 grudnia

---
+++ Assets/myVR/Scripts/Fade.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fade : MonoBehaviour {
+
+    private CanvasGroup canvasGroup;
+
+    void Awake() {
+        canvasGroup = GetComponent<CanvasGroup>();
+        //canvasGroup.alpha = 100;
+        //fadeMe();
+    }
+
+    public void fadeMe() {
+        
+        StartCoroutine(makeFade());
+    }
+
+    IEnumerator makeFade() {
+        while (canvasGroup.alpha < 1) {
+            canvasGroup.alpha += Time.deltaTime / 2;
+            yield return null;
+        }
+        canvasGroup.interactable = false;
+        yield return null;
+    }
+}
